4ca683a7ceb22976352519820089e3233b8da6f1
meinardus <brya.seran@gmail.com>
2026-05-16 20:38:11 +0700

    fix productions

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,17 @@ DEBUG = False
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default=".karyawijaya.com",
+    default=".karyawijaya.com,.portal.kawi-nickel.com",
 ).split(",")
 
 CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://apps.portal.kawi-nickel.com",
+    "https://karyawijaya.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://apps.portal.kawi-nickel.com",
+    "https://karyawijaya.com",
+]
